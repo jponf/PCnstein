fb041ba20a020409237f3aeb1a232f0742e825c0
Josep Pon Farreny <ponpepo@gmail.com>
2013-03-28 22:47:04 +0100

    Added __unicode__ method to models (Used in django admin)

--- PCnsteinapp/models.py
+++ PCnsteinapp/models.py
@@ -6,8 +6,11 @@ from django.db import models
 #
 class Manufacturer(models.Model):
 	name = models.CharField(max_length=35)
-	ref = models.CharField(max_length=20)
 	desc = models.TextField()
+
+	def __unicode__(self):
+		return self.name
+
 
 #
 #
@@ -16,11 +19,19 @@ class Component(models.Model):
 	name = models.CharField(max_length=75)
 	desc = models.TextField()
 	avg_price = models.DecimalField(max_digits=7, decimal_places=3)
+	img = models.ImageField(upload_to='static/img')
+	category = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.name + ' - ' + ref
 
 #
 #
 class OperatingSystem(models.Model):
 	name = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return self.name
 
 #
 #
@@ -29,6 +40,9 @@ class SupportedBy(models.Model):
 	os = models.ForeignKey(OperatingSystem)
 	min_version = models.CharField(max_length=20)
 	max_version = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return str(component) + ' supported by ' + str(os)
 
 #
 #
@@ -41,3 +55,13 @@ class CMadeBy(models.Model):
 class OSMadeBy(models.Model):
 	os = models.ForeignKey(OperatingSystem)
 	manufacturer = models.ForeignKey(Manufacturer)
+
+#
+# Component Generalization
+class CPU(models.Model):
+	family = models.CharField(max_length=25)
+	socket = models.CharField(max_length=15)
+	arch = models.IntegerField()
+	cores = models.IntegerField()
+	clock_speed = models.FloatField()
+	arch_size = models.IntegerField()
