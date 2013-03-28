2a121479f96505a43eb25d19f55f3b66fa41f3c9
Josep Pon Farreny <ponpepo@gmail.com>
2013-03-28 21:51:27 +0100

    Enabled django admin interface

---
+++ PCnsteinapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from PCnsteinapp.models import Manufacturer, Component, OperatingSystem, \
+								SupportedBy, CMadeBy, OSMadeBy
+
+admin.site.register(Manufacturer)
+admin.site.register(Component)
+admin.site.register(OperatingSystem)
+admin.site.register(SupportedBy)
+admin.site.register(CMadeBy)
+admin.site.register(OSMadeBy)
