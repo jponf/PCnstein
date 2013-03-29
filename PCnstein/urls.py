3f8a2132ce013b1198aef8c29052fad0288c8b5b
Josep Pon Farreny <ponpepo@gmail.com>
2013-03-29 11:56:04 +0100

    Added Templates and testing rest urls

--- PCnstein/urls.py
+++ PCnstein/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from PCnsteinapp.views import GetComponents, GetComponent
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^components/', GetComponents),
+    url(r'^components/()', GetComponent),
+    
 )
--- PCnsteinapp/views.py
+++ PCnsteinapp/views.py
@@ -1,1 +1,45 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template.loader import render_to_string
+from django.utils import simplejson
+from django.http import HttpResponse
+from django.core import serializers
+
+import models
+
+#
+#
+def GetComponentsInfoAsList():
+	cinf = []
+
+	for c in models.Component.objects.all():
+		cinfo = { 'ref' : c.ref, 'name' : c.name, 'desc' : c.desc, 
+				  'avg_price' : str(c.avg_price), 'category' : c.category }
+		cinf.append(cinfo)
+	return cinf
+
+#
+#
+def GetComponents(request):
+	format = request.GET.get('format', 'http')
+
+	if format == 'json':
+		#json = serializers.serialize('json', models.Component.objects.all())
+		#return HttpResponse(json, mimetype='application/json')
+		return HttpResponse(simplejson.dumps(GetComponentsInfoAsList()),
+							mimetype='application/json')
+		
+	elif format == 'xml':
+		# xml = serializers.serialize('xml', models.Component.objects.all())
+		# return HttpResponse(xml, mimetype='application/xml')
+		return HttpResponse(render_to_string('components.xml', 
+				{'components': models.Component.objects.all()}))
+	elif format == 'http':
+		return HttpResponse('HTTP')
+	else:
+		return HttpResponse('Error madafaka')
+
+#
+#
+def GetComponent(request, ref):
+	return HttpResponse(ref)
