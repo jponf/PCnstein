01af6bcf0b01b967111425ed6f2c7bce1cf930da
Marc Piñol <marcpinolpueyo@gmail.com>
2013-04-01 10:59:06 +0200

    Added manufacturer data fetchers to datautils.py Added manufacturer urls parsing to urls.py Added manufacturer url as a constant to globdata.py

--- PCnsteinapp/datautils.py
+++ PCnsteinapp/datautils.py
@@ -37,5 +37,36 @@ def GetComponentInfo(ref):
 		}
 	]
 
+	return cinf
+
+#
+#
+def GetManufacturersInfoAsList():
+
+	manu_info = []
+
+	for m in models.Manufacturer.objects.all():
+		single_manu_info = { 
+						'name': m.name, 
+						'desc': m.desc, 
+						'links': { 
+									'rel': self, 
+									'href': '%s/%s/%s' % (globdata.API_URL, globdata.API_MANUFACTURERS, m.ref)
+								 }
+					}
+
+		manu_info.append(single_manu_info)
+
+	return manu_info
+
+#
+#
+def GetManufacturersInfo(name):
+
+	manufacturer = models.Manufacturer.objects.get(pk=name)
+
+	manu_info = [{'name': manufacturer.name, 'desc': manufacturer.desc}, 'links': {'rel': 'self'}]
+
+	return manu_info
 
 
--- PCnsteinapp/globdata.py
+++ PCnsteinapp/globdata.py
@@ -5,3 +5,6 @@ API_URL = 'http://localhost:8000'
 
 # Api components entry url
 API_COMPONENTS = 'components'
+
+# Api manuufacturers entry url
+API_MANUFACTURERS = 'manufacturers'
