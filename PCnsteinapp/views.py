f9634a3dd1649932bb1e30bffdd9a66386302d29
Josep Pon Farreny <ponpepo@gmail.com>
2013-04-01 11:02:02 +0200

    Added templates for manufacturers request with xml format Added view for api requests /manufacturers and /manufacturers/name
    	new file:   PCnsteinapp/templates/manufacturer.xml
    	new file:   PCnsteinapp/templates/manufacturers.xml
    	modified:   PCnsteinapp/views.py

--- PCnsteinapp/views.py
+++ PCnsteinapp/views.py
@@ -1,9 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+from django.core import serializers
+from django.utils import simplejson
 from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
-from django.utils import simplejson
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.core import serializers
+from django.core.exceptions import ObjectDoesNotExist
 
 import models
 import datautils
@@ -12,46 +13,105 @@ import datautils
 #
 #
 def GetMainPage(request):
-	format = request.GET.get('format', 'http')
-
 	return render_to_response('mainpage.html')
 
 #
 #
 def GetComponents(request):
-	format = request.GET.get('format', 'http')
+	format = request.GET.get('format', 'html')
 
 	if format == 'json':
-		#json = serializers.serialize('json', models.Component.objects.all())
-		#return HttpResponse(json, mimetype='application/json')
-		return HttpResponse(
+		return HttpResponse (
 			simplejson.dumps( datautils.GetComponentsInfoAsList() ),
-			mimetype='application/json' )
+			mimetype='application/json' 
+							)
 		
 	elif format == 'xml':
-		# xml = serializers.serialize('xml', models.Component.objects.all())
-		# return HttpResponse(xml, mimetype='application/xml')
-		return HttpResponse(render_to_string('components.xml', 
-				{'components': models.Component.objects.all()}))
-	elif format == 'http':
+		params = { 'components': models.Component.objects.all() }
+		return HttpResponse ( 
+			render_to_string('components.xml', params), 
+			mimetype='application/xml'
+							)
+
+	elif format == 'html':
 		params = { 'pagetitle' : "Components"}
 		params['cinfo'] = datautils.GetComponentsInfoAsList()
-
 		return render_to_response('components.html', params)
+
 	else:
-		return HttpResponseBadRequest('Bad Format')
+		return HttpResponseBadRequest(
+			'Wrong parameter "format" [html, xml, json]')
 
 #
 #
 def GetComponent(request, ref):
-	format = request.GET.get('format', 'http')
+	format = request.GET.get('format', 'html')
 
 	if format == 'json':
 		return HttpResponse(
 			simplejson.dumps( datautils.GetComponentInfo(ref)) )
+
 	elif format == 'xml':
-		pass # TODO
-	elif format == 'http':
-		pass # TODO
+		return HttpResponse('XML for component ' + ref)
+
+	elif format == 'html':
+		return HttpResponse('HTML for component ' + ref)
+
 	else:
-		return HttpResponseBadRequest('Bad Format')
+		return HttpResponseBadRequest(
+			'Wrong parameter "format" [html, xml, json]')
+
+#
+#
+def GetManufacturers(request):
+	format = request.GET.get('format', 'html')
+
+	if format == 'json':
+		return HttpResponse (
+			simplejson.dumps(datautils.GetManufacturersInfoAsList() ),
+			mimetype='application/json'
+							)
+	elif format == 'xml':
+		params = { 'manufacturers' : models.Manufacturer.objects.all() }
+		return HttpResponse (
+			render_to_string('manufacturers.xml', params),
+			mimetype='application/xml'
+							)
+
+	elif format == 'http'
+		return HttpResponse("HTML for manufacturers :D")
+
+	else:
+		return HttpResponseBadRequest(
+			'Wrong parameter "format" [html, xml, json]')
+
+
+#
+#
+def GetManufacturer(request, name):
+	format = request.GET.get('format', 'html')
+
+	try:
+
+		if format == 'json':
+			return HttpResponse (
+				simplejson.dumps(datautils.GetManufacturersInfoAsList() ),
+				mimetype='application/json'
+								)
+		elif format == 'xml':
+			params = { 'manufacturers' : models.Manufacturer.objects.all() }
+			return HttpResponse (
+				render_to_string('manufacturers.xml', params),
+				mimetype='application/xml'
+								)
+
+		elif format == 'http'
+			return HttpResponse("HTML for manufacturers :D")
+
+		else:
+			return HttpResponseBadRequest(
+				'Wrong parameter "format" [html, xml, json]')
+
+	except ObjectDoesNotExist, e:
+		return HttpResponseNotFound(
+			"Manufacturer with name: " + name + ", does not exists")
